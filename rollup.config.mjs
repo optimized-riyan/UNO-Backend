e6766ca362e6bd8442f9eeaa520494dc12685ed9
Riyan Gonsalves <riyan0350@gmail.com>
2025-01-12 02:12:09 +0530

    build: stuff for prod build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,11 +3,22 @@ import commonjs from "@rollup/plugin-commonjs";
 import { defineConfig } from "rollup";
 import terser from "@rollup/plugin-terser";
 import json from "@rollup/plugin-json";
+import replace from "@rollup/plugin-replace";
+import dotenv from 'dotenv';
+
+const env = dotenv.config({
+    path: `./.env.production`
+}).parsed;
+
+const replacements = Object.keys(env).reduce((acc, key) => {
+  acc[`process.env.${key}`] = JSON.stringify(env[key]);
+  return acc;
+}, {});
 
 export default defineConfig({
     input: 'dist/app.js',
     output: {
-        file: 'bundle.js',
+        file: 'bundle.mjs',
         format: 'esm',
         name: 'bundle',
         plugins: [terser()]
@@ -16,5 +27,9 @@ export default defineConfig({
         nodeResolve(),
         commonjs(),
         json(),
+        replace({
+            preventAssignment: true,
+            ...replacements
+        }),
     ]
 });
